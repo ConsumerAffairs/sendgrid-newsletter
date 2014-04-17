acace183d22f44a420852de6fe397525d90df65d
Buddy Lindsey <buddy@buddylindsey.com>
2014-04-17 15:29:04 -0500

    add setup file to be installable appe

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='sendgrid-newsletter',
+    version='0.1',
+    author='Buddy Lindsey, Jr.',
+    author_email='blindsey@consumeraffairs.com',
+    url='https://github.com/ConsumerAffairs/sendgrid-newsletter',
+    description='Library for using SendGrids newsletter api',
+    install_requires=['requests==2.2.1'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Marketing :: Newsletters'
+    ],
+    packages=['sendgridnewsletter'],
+    long_description=read('README.md')
+)
